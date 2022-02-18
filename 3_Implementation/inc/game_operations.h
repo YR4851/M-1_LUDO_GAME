cbbaad64d8d02777231666ce8e591e0af25faaa3
Yash Rastogi <87801737+YR4851@users.noreply.github.com>
2022-02-18 23:12:32 +0530

    Create game_operations.h

---
+++ 3_Implementation/inc/game_operations.h
@@ -0,0 +1,9 @@
+/** 
+* @file calculator_operations.h
+* Calculator application with 4 mathematical operations
+*
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include <time.h>
+int check_error(int a);
